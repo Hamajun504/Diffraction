da55c4ed8d147e471dd009b705b040544b08d458
Yaroslav Orlov <orlov.iaa@phystech.edu>
2023-12-08 13:37:24 +0300

    test out

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "screen.cpp"
+#include <fstream>
 
 int main()
 {
@@ -16,6 +17,17 @@ int main()
     
     screen.evaluate_phase(source);
 
+    std::ofstream out("buffer_image.csv");
+    auto image = screen.normalized_intensity();
+    for (size_t i = 0; i < image.size(); ++i)
+        for (size_t j = 0; j < image[0].size(); ++j) {
+            out << image[i][j];
+            if (j + 1 == image[0].size())
+                out << std::endl;
+            else
+                out << ',';
+        }
+    out.close();
 
     return 0;
 }
